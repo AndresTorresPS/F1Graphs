379558e9fa883552299008e892633cee4f98c747
AndresTorres <andres.torres@peoplesvoice.co>
2025-05-16 23:21:12 -0500

    Graphs from csvs

--- Modeling/F1GraphTest.py
+++ Modeling/F1GraphTest.py
@@ -1,8 +1,61 @@
-a = 5
-b = 2
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
 
-c = a + b
-print(c)
+def new_di_graph_unweighted(adjacency_df):
+    """
+    Crea un grafo dirigido sin pesos a partir de un DataFrame de adyacencia.
+    Se agrega una arista si el valor es distinto de cero.
+    """
+    G = nx.DiGraph()
+    for source in adjacency_df.index:
+        for target in adjacency_df.columns:
+            if adjacency_df.loc[source, target] != 0:
+                G.add_edge(source, target)
+    return G
 
+def new_di_graph(adjacency_df):
+    """
+    Crea un grafo dirigido y ponderado a partir de un DataFrame de adyacencia.
+    Los valores del DataFrame se usan como pesos de las aristas.
+    """
+    G = nx.DiGraph()
+    for source in adjacency_df.index:
+        for target in adjacency_df.columns:
+            weight = adjacency_df.loc[source, target]
+            if weight != 0:
+                G.add_edge(source, target, weight=weight)
+    return G
 
+def print_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw(graph, pos, with_labels=True, node_color='lightblue', edge_color='gray', arrows=True)
+    plt.show()
+
+'''
+# Ejemplo: Crear un DataFrame 5x5 de adyacencia con nombres personalizados
+nombres = ['A', 'B', 'C', 'D', 'E']
+df_adyacencia = pd.DataFrame([
+    [0, 1, 0, 0, 1],
+    [0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0]
+], index=nombres, columns=nombres)
+
+grafo = new_di_graph(df_adyacencia)
+visualizar_grafo(grafo)
+'''
+
+'''
+# Leer la matriz de adyacencia desde el archivo CSV sin tomar en cuenta los pesos
+adjacency_df = pd.read_csv('Mark01.csv', index_col=0)
+mark_01_graph = new_di_graph_unweighted(adjacency_df)
+print_graph(mark_01_graph)
+'''
+
+# Leer la matriz de adyacencia desde el archivo CSV (incluyendo encabezados)
+adjacency_df = pd.read_csv('Mark01.csv', index_col=0)
+mark_01_graph = new_di_graph(adjacency_df)
+print_graph(mark_01_graph)
 
---
+++ Modeling/test_F1GraphTest.py
@@ -0,0 +1,44 @@
+import networkx as nx
+from F1GraphTest import new_di_graph
+
+def test_new_di_graph_empty():
+    matriz = []
+    G = new_di_graph(matriz)
+    assert isinstance(G, nx.DiGraph)
+    assert G.number_of_nodes() == 0
+    assert G.number_of_edges() == 0
+
+def test_new_di_graph_single_node():
+    matriz = [[0]]
+    G = new_di_graph(matriz)
+    assert G.number_of_nodes() == 1
+    assert G.number_of_edges() == 0
+
+def test_new_di_graph_simple():
+    matriz = [
+        [0, 1],
+        [0, 0]
+    ]
+    G = new_di_graph(matriz)
+    assert set(G.nodes) == {0, 1}
+    assert set(G.edges) == {(0, 1)}
+
+def test_new_di_graph_multiple_edges():
+    matriz = [
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0]
+    ]
+    G = new_di_graph(matriz)
+    assert set(G.nodes) == {0, 1, 2}
+    assert set(G.edges) == {(0, 1), (1, 2), (2, 0)}
+
+def test_new_di_graph_self_loop():
+    matriz = [
+        [1, 0],
+        [0, 0]
+    ]
+    G = new_di_graph(matriz)
+    assert (0, 0) in G.edges
+    assert G.number_of_edges() == 1
+
